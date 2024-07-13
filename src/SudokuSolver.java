269080ffcddda596743f35f62972fbbc89a246d9
𝚃𝚞𝚜𝚑𝚊𝚛 𝚂𝚒𝚗𝚐𝚑 <142152854+tusharsinghgta001@users.noreply.github.com>
2024-07-13 14:05:12 +0100

    First

---
+++ src/SudokuSolver.java
@@ -0,0 +1,85 @@
+public class SudokuSolver {
+
+    private static final int SIZE = 9;
+    private static final int SUBGRID_SIZE = 3;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check the row
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // Check the column
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check the 3x3 subgrid
+        int startRow = row - row % SUBGRID_SIZE;
+        int startCol = col - col % SUBGRID_SIZE;
+        for (int i = 0; i < SUBGRID_SIZE; i++) {
+            for (int j = 0; j < SUBGRID_SIZE; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
